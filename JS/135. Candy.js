ccdef75b596074dfd044c3a4d7c5d33b1a029827
Hữu Phước <tpgnor@gmail.com>
2025-04-21 18:45:44 +0700

    135. Candy solved

---
+++ JS/135. Candy.js
@@ -0,0 +1,33 @@
+// Greedy from the left to right => if curr's rating > left'rating and curr's candies <= left's candies
+// => curr's candies = left's candies + 1
+
+// Greedy from the right to left => if curr's rating > right'rating and curr's candies <= right's candies
+// => curr's candies = right's candies + 1
+
+/**
+ * @param {number[]} ratings
+ * @return {number}
+ */
+const candy = (ratings) => {
+  const childWithCandies = new Array(ratings.length).fill(1);
+  for (let i = 1; i < ratings.length; i++) {
+    if (
+      ratings[i] > ratings[i - 1] &&
+      childWithCandies[i] <= childWithCandies[i - 1]
+    ) {
+      childWithCandies[i] = childWithCandies[i - 1] + 1;
+    }
+  }
+  for (let i = ratings.length - 2; i >= 0; i--) {
+    if (
+      ratings[i] > ratings[i + 1] &&
+      childWithCandies[i] <= childWithCandies[i + 1]
+    ) {
+      childWithCandies[i] = childWithCandies[i + 1] + 1;
+    }
+  }
+  return childWithCandies.reduce((total, child) => total + child, 0);
+};
+
+const ratings = [1, 2, 2];
+console.log(candy(ratings));
