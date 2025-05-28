290dac0097913f33f02ce3cb4cdfa2bcee4a57ea
Hữu Phước <tpgnor@gmail.com>
2025-05-28 11:20:06 +0700

    125. Valid Palindrome

---
+++ JS/125. Valid Palindrome.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+const isPalindrome = (s) => {
+  const isAlphanumeric = (char) => {
+    return /^[a-zA-Z0-9]$/.test(char);
+  };
+  let l = 0,
+    r = s.length - 1;
+  while (l < r) {
+    const leftChar = s[l].toLowerCase();
+    const rightChar = s[r].toLowerCase();
+    if (!isAlphanumeric(leftChar)) {
+      l++;
+      continue;
+    }
+    if (!isAlphanumeric(rightChar)) {
+      r--;
+      continue;
+    }
+    if (leftChar !== rightChar) {
+      return false;
+    }
+    l++;
+    r--;
+  }
+  return true;
+};
+
+const s = " ";
+console.log(isPalindrome(s));
