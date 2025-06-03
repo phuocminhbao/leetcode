a4935508d70ddafd60e1c21d7376123e437c0c64
Hữu Phước <tpgnor@gmail.com>
2025-06-03 16:18:26 +0700

    155. Min Stack

---
+++ JS/155. Min Stack.js
@@ -0,0 +1,40 @@
+class MinStack {
+  #stack;
+  #minStack;
+  constructor() {
+    this.#stack = [];
+    this.#minStack = [];
+  }
+  push(item) {
+    this.#stack.push(item);
+    if (item <= (this.getMin() ?? Infinity)) {
+      this.#minStack.push(item);
+    }
+  }
+  pop() {
+    if (this.top() === this.getMin()) {
+      this.#minStack.pop();
+    }
+    this.#stack.pop();
+  }
+  top() {
+    return this.#stack[this.#stack.length - 1];
+  }
+  getMin() {
+    return this.#minStack[this.#minStack.length - 1];
+  }
+}
+
+const main = () => {
+  const actions = ["push", "push", "push", "getMin", "pop", "top", "getMin"];
+  const values = [[-2], [0], [-3], [], [], [], []];
+  const res = [];
+  const minStack = new MinStack();
+  actions.forEach((action, index) => {
+    const actionRes = minStack[action](...values[index]);
+    res.push(actionRes);
+  });
+  console.log(res);
+};
+
+main();
