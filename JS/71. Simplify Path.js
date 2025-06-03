751c0bbe9bfe696133c370c966a4010a055bf921
Hữu Phước <tpgnor@gmail.com>
2025-06-03 11:50:06 +0700

    71. Simplify Path

---
+++ JS/71. Simplify Path.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} path
+ * @return {string}
+ */
+const simplifyPath = (path) => {
+  const simplifiedCanonicalPath = [];
+  const filesOrDirectories = path.split("/");
+  filesOrDirectories.forEach((item) => {
+    if (["", "."].includes(item)) {
+      return;
+    }
+    if (item === "..") {
+      simplifiedCanonicalPath.pop();
+      return;
+    }
+    simplifiedCanonicalPath.push(item);
+  });
+  return `/${simplifiedCanonicalPath.join("/")}`;
+};
+
+const path = ".";
+console.log(simplifyPath(path));
