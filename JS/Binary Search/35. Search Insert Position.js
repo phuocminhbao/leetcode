eb5eeff86b43fa806e2497a25b2cb8e3a32acc53
Hữu Phước <tpgnor@gmail.com>
2025-06-06 15:52:01 +0700

    35. Search Insert Position

---
+++ JS/Binary Search/35. Search Insert Position.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+const searchInsert = (nums, target) => {
+    let l = 0,
+        r = nums.length - 1;
+    while (l <= r) {
+        const mid = Math.floor((l + r) / 2);
+        const midNumber = nums[mid];
+        if (midNumber === target) {
+            return mid;
+        }
+        if (midNumber > target) {
+            r = mid - 1;
+            continue;
+        }
+        l = mid + 1;
+    }
+    return l;
+};
+
+const nums = [2, 7, 8, 9, 10];
+const target = 0;
+
+console.log(searchInsert(nums, target));
