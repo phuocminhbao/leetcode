2e8af6420e29d241fff8583b1a7a765e16d27c56
Hữu Phước <tpgnor@gmail.com>
2025-11-05 17:21:37 +0700

    200. Number of Islands

--- JS/Graph/200. Number of Islands.js
+++ JS/Graph/200. Number of Islands.js
@@ -1,24 +1,80 @@
-import { DirectedGraph } from "datastructures-js";
-
 /**
  * @param {character[][]} grid
  * @return {number}
  */
-const numIslands = (grid) => {
-    const graph = new DirectedGraph();
-    graph
-        .addVertex("v1", 1)
-        .addVertex("v2", 2)
-        .addVertex("v3", 3)
-        .addVertex("v4", 4)
-        .addVertex("v5", 5);
-    debugger;
+const numIslandsDFS = (grid) => {
+    const ROW_LENGTH = grid.length;
+    const COL_LENGTH = grid[0].length;
+    let lands = 0;
+    const dfs = (row, col) => {
+        const value = grid[row]?.[col] ?? "0";
+        if (value === "0") {
+            return;
+        }
+        grid[row][col] = "0";
+        dfs(row + 1, col);
+        dfs(row, col + 1);
+        dfs(row - 1, col);
+        dfs(row, col - 1);
+    };
+    for (let row = 0; row < ROW_LENGTH; row++) {
+        for (let col = 0; col < COL_LENGTH; col++) {
+            const value = grid[row][col];
+            if (value === "0") {
+                continue;
+            }
+            lands++;
+            dfs(row, col);
+            grid[row][col] = "0";
+        }
+    }
+    return lands;
 };
+
+const numIslandsBFS = (grid) => {
+    const ROW_LENGTH = grid.length;
+    const COL_LENGTH = grid[0].length;
+    let lands = 0;
+    const debug = [];
+    const bfs = (row, col) => {
+        grid[row][col] = "0";
+        const queue = [[row, col]];
+        while (queue.length > 0) {
+            const [currRow, currCol] = queue.shift();
+            [
+                [0, 1],
+                [1, 0],
+                [0, -1],
+                [-1, 0],
+            ].forEach(([nextRow, nextCol]) => {
+                const value =
+                    grid[currRow + nextRow]?.[currCol + nextCol] ?? "0";
+                if (value === "1") {
+                    grid[currRow + nextRow][currCol + nextCol] = "0";
+                    queue.push([currRow + nextRow, currCol + nextCol]);
+                }
+            });
+        }
+    };
+    for (let row = 0; row < ROW_LENGTH; row++) {
+        for (let col = 0; col < COL_LENGTH; col++) {
+            const value = grid[row][col];
+            if (value === "0") {
+                continue;
+            }
+            lands++;
+            debug.push(`${row}-${col}`);
+            bfs(row, col);
+        }
+    }
+    return lands;
+};
+
 const grid = [
-    ["1", "1", "0", "0", "0"],
-    ["1", "1", "0", "0", "0"],
-    ["0", "0", "1", "0", "0"],
-    ["0", "0", "0", "1", "1"],
+    ["1", "0", "1"],
+    ["0", "1", "0"],
+    ["1", "0", "1"],
 ];
 
-const res = numIslands(grid);
+const res = numIslandsDFS(grid);
+console.log(res);
